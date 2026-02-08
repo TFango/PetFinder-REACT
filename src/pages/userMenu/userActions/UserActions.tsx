79f13579a1445e966f9717b573a468aa826cd84e
Emanuel Bustos <fakuj305@gmail.com>
2026-02-08 17:34:43 -0300

    change all repo

--- src/pages/userMenu/userActions/UserActions.tsx
+++ src/pages/userMenu/userActions/UserActions.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button } from "../../../ui/button/";
+import { Button } from "../../../ui/button";
 
 import styles from "./UserActions.module.css";
 import { useNavigate } from "react-router-dom";
